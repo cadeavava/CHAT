61cb3cf3ceb96b6e1b37b2208cb7c6388480f945
cadeavava <fischermartins2007@gmail.com>
2024-06-27 14:06:00 -0300

    Base da API

---
+++ src/api.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+const router = express.Router();
+app.use('/', router.get('/', (req, res) => {
+    res.status(200).send("<h1>API - CHAT</h1>")
+}));
+
+app.use('/', router.get('/sobre', (req, res, next) => {
+    res.status(200).send({
+        "nome": "API - CHAT",
+        "versao": "0.1.0",
+        "author": "Valentina Fischer Martins Pacheco"
+    })
+}));
+
+module.exports=app;
